760061a5767c494e4e7ae6fb6dbe32fa6f187d58
dlrbcnvk <dlrbcnvk@naver.com>
2023-08-12 21:53:08 +0900

    first commit

---
+++ src/main/java/com/example/reactortutorial/ReactorTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactortutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactorTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reactortutorial/ReactorTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactortutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactorTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
